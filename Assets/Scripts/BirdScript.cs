748a92ba54934106210ad628c4149a48204c7383
Alhvi Balcarcel <abalcarcel@gmail.com>
2018-01-24 01:19:24 -0600

    se agregaron scripts necesarios para hacer el lab

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+
+    public float jumpForce = 200f;
+    private Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        if (Input.GetButtonDown("Jump")) {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+	}
+
+}
---
+++ Assets/Scripts/Column.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Column : MonoBehaviour {
+
+    private float scrollingSpeed = 2f;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        transform.Translate(Vector3.left * scrollingSpeed * Time.deltaTime);
+        if (transform.position.x < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
